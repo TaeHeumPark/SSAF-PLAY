a06ad3ea5bfef02c95945f3798c4fd39c1e01797
xogma <xogmamoc@naver.com>
2023-05-23 09:15:34 +0900

    UserMatch dto 출력을 위한 변수들 추가

--- project/Back/Ssaf_play/src/main/java/com/ssaf/play/dto/UserMatch.java
+++ project/Back/Ssaf_play/src/main/java/com/ssaf/play/dto/UserMatch.java
@@ -8,6 +8,17 @@ public class UserMatch {
 	private int match_positions; // 매칭 되는 경기의 position	
 	private int team; // 1팀인지 2팀인지
 	
+	// 출력을 위한
+	private String name;
+	private String profile_img;
+	private int match_count;
+	private String record;
+	private String position_name;
+	private String grade_name;
+	// match_positions
+	// team
+	// 두 개는 위에 있음.
+	
 	public UserMatch() {
 		// TODO Auto-generated constructor stub
 	}
@@ -19,6 +30,21 @@ public class UserMatch {
 		this.match_id = match_id;
 		this.match_positions = match_positions;
 		this.team = team;
+	}
+	
+	
+
+	public UserMatch(int match_positions, int team, String name, String profile_img, int match_count, String record,
+			String position_name, String grade_name) {
+		super();
+		this.match_positions = match_positions;
+		this.team = team;
+		this.name = name;
+		this.profile_img = profile_img;
+		this.match_count = match_count;
+		this.record = record;
+		this.position_name = position_name;
+		this.grade_name = grade_name;
 	}
 
 	public int getUser_match_id() {
@@ -60,10 +86,62 @@ public class UserMatch {
 	public void setTeam(int team) {
 		this.team = team;
 	}
+	
+	// ---
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getProfile_img() {
+		return profile_img;
+	}
+
+	public void setProfile_img(String profile_img) {
+		this.profile_img = profile_img;
+	}
+
+	public int getMatch_count() {
+		return match_count;
+	}
+
+	public void setMatch_count(int match_count) {
+		this.match_count = match_count;
+	}
+
+	public String getRecord() {
+		return record;
+	}
+
+	public void setRecord(String record) {
+		this.record = record;
+	}
+
+	public String getPosition_name() {
+		return position_name;
+	}
+
+	public void setPosition_name(String position_name) {
+		this.position_name = position_name;
+	}
+
+	public String getGrade_name() {
+		return grade_name;
+	}
+
+	public void setGrade_name(String grade_name) {
+		this.grade_name = grade_name;
+	}
 
 	@Override
 	public String toString() {
 		return "UserMatch [user_match_id=" + user_match_id + ", user_id=" + user_id + ", match_id=" + match_id
-				+ ", match_positions=" + match_positions + ", team=" + team + "]";
+				+ ", match_positions=" + match_positions + ", team=" + team + ", name=" + name + ", profile_img="
+				+ profile_img + ", match_count=" + match_count + ", record=" + record + ", position_name="
+				+ position_name + ", grade_name=" + grade_name + "]";
 	}
 }
